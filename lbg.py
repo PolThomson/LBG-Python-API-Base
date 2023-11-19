fcbdee33b7858fe022e0ee61b5b0f16ea9f4b024
Paul Thomson <paulthomson71@gmail.com>
2023-11-19 16:45:25 +0000

    done

--- lbg.py
+++ lbg.py
@@ -5,6 +5,7 @@ LBG learning-oriented CRUD-based RESTful API using standard Flask routing
 # import Flask microframework and associated tools
 from flask import Flask, request, jsonify
 from flask_api import status
+from os import getenv
 
 # import SQL Alchemy (including ORM - Object-relational Mapper - and its data mapper pattern)
 from models import db, ItemModel
@@ -15,7 +16,6 @@ import mimetypes
 mimetypes.add_type('text/javascript', '.js')
 
 # set up the app with listening socket for http requests and appropriate hostname
-PORT = 8080
 HOST = '0.0.0.0'
 
 # get app to serve static files from the public directory
@@ -205,5 +205,6 @@ def delete_one(_id):
 # module import protection
 if __name__ == '__main__':
     # get app to serve
+    PORT = getenv("PORT", 8080)
     print(f'API Listening on http://{HOST}:{PORT}')
     app.run(host=HOST, port=PORT, debug=True)
